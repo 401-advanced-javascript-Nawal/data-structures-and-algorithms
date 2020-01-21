dbbc9ef593e5f7205d6134031b97a81f741fa490
Nsuliman <nsuliman88@gmail.com>
2020-01-22 00:28:55 +0200

    Code challenge Done

---
+++ challenges/arrayBinarySearch/arrayBinarySearch.js
@@ -0,0 +1,38 @@
+'use strict';
+
+let binValidator = module.exports = {};
+
+
+let searchArr = [4, 8, 15, 16, 23, 42];
+let searchKey = 23;
+
+// console.log('searchArr : ', searchArr);
+// console.log('searchKey : ', searchKey);
+
+binValidator.binarySearch =(array, key) =>{
+
+    let intialIdx = 0;
+    let endIdx = array.length-1;
+
+    while (intialIdx <= endIdx) {
+
+        let middle = Math.floor((intialIdx + endIdx) / 2);
+        // console.log('middle : ', middle);
+
+        if (array[middle] === key)
+            return middle;
+
+        if (array[middle] < key) {
+            intialIdx = middle + 1;
+            console.log('intialIdx : ', intialIdx);
+        }
+        else {
+            endIdx = middle - 1;
+            console.log('endIdx : ', endIdx);
+        }
+    }
+    return false;
+}
+
+// binarySearch(searchArr, searchKey)
+// console.log('binarySearch(searchArr, searchKey) : ', binarySearch(searchArr, searchKey));
