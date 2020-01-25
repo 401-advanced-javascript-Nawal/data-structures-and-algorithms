8203fc5c7c8ef15d0c4dc69cebc95118dd27d67b
Nsuliman <nsuliman88@gmail.com>
2020-01-25 22:25:34 +0200

    Insert testing Completed

--- __tests__/lib/linkedList.test.js
+++ __tests__/lib/linkedList.test.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const LinkedList = require('../../lib/linkedList.js');
+const Node = require('../../lib/node.js');
+
 
 describe(' LinkedList Module ', () => {
 
@@ -10,5 +12,11 @@ describe(' LinkedList Module ', () => {
         expect(testLL.head).toEqual(null);
     });
 
-    
+    it(' Insert new node with value ', () => {
+
+        let value = 'Nawal' ;
+        let testLL = new LinkedList();
+        expect(testLL.insert(value)).toEqual(testLL);
+    });
+
 }); // end of LinkedList Module
