6916e56a570053d4cb38084363ec600da33fbfd4
Nsuliman <nsuliman88@gmail.com>
2020-01-25 22:05:11 +0200

    node testing Done

---
+++ __tests__/lib/linkedList.test.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const LinkedList = require('../../lib/linkedList.js');
+
+describe(' LinkedList Module ', () => {
+
+    it(' LinkedList Constructor ', () => {
+
+        let testLL = new LinkedList();
+        
+
+    });
+
+}); // end of LinkedList Module
