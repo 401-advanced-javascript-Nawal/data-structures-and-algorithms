e4eb5b76c06fee016e2d24d7563c07f94f8e24da
Nsuliman <nsuliman88@gmail.com>
2020-01-25 22:10:50 +0200

    linkedList testing starts

--- __tests__/lib/linkedList.test.js
+++ __tests__/lib/linkedList.test.js
@@ -7,8 +7,8 @@ describe(' LinkedList Module ', () => {
     it(' LinkedList Constructor ', () => {
 
         let testLL = new LinkedList();
-        
-
+        expect(testLL.head).toEqual(null);
     });
 
+    
 }); // end of LinkedList Module
