9e2a8239d6b2f2eb84a9cd914aaddfd940d2a859
Nsuliman <nsuliman88@gmail.com>
2020-01-27 17:30:32 +0200

    InsertBefore method testing Done

--- __tests__/lib/linkedList.test.js
+++ __tests__/lib/linkedList.test.js
@@ -41,9 +41,15 @@ describe(' LinkedList Module ', () => {
         expect(testLL.head.value).toEqual(value);
         });
 
-    // it(' insertBefore() Insert Node before another Node ', () => {
-    
-    // });
+    it(' insertBefore() Insert Node before another Node ', () => {
+        let value1 = 'N1' ;
+        let value2 = 'N2' ;
+        testLL.insert(value1);
+        testLL.insert(value2);
+        testLL.insertBefore(value1,value2);
+        expect(testLL.head.value).toEqual(value2);
+        // expect(testLL.head.next).toEqual(value2)
+    });
 
     // it(' insertAfter() Insert node after given value ', () => {
     
