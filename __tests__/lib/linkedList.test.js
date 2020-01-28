d3b5e876d3d2d6f21e7f126b30a03792ae5f25d0
Nsuliman <nsuliman88@gmail.com>
2020-01-28 18:52:54 +0200

    eslint errors handled

--- __tests__/lib/linkedList.test.js
+++ __tests__/lib/linkedList.test.js
@@ -3,54 +3,54 @@
 const LinkedList = require('../../lib/linkedList.js');
 
 describe(' LinkedList Module ', () => {
-    let testLL = new LinkedList();
-    it(' LinkedList Constructor ', () => {
-        expect(testLL.head).toEqual(null);
-    });
+  let testLL = new LinkedList();
+  it(' LinkedList Constructor ', () => {
+    expect(testLL.head).toEqual(null);
+  });
 
-    it(' Insert() new node with value ', () => {
-        let value = 'Nawal';
-        testLL.insert(value);
-        expect(testLL.head.value).toEqual(value);
-        expect(typeof (testLL.head.next)).toEqual('object');
-    });
+  it(' Insert() new node with value ', () => {
+    let value = 'Nawal';
+    testLL.insert(value);
+    expect(testLL.head.value).toEqual(value);
+    expect(typeof (testLL.head.next)).toEqual('object');
+  });
 
-    it(' Includes() node\'s value if exist ', () => {
-        let value = 'Nawal';
-        testLL.includes(value);
-        expect(testLL.head.value).toEqual(value);
-    });
+  it(' Includes() node\'s value if exist ', () => {
+    let value = 'Nawal';
+    testLL.includes(value);
+    expect(testLL.head.value).toEqual(value);
+  });
 
-    // it(' ToString nodes ', () => {
-    //     let value = 'Nawal' ;
-    //     let testLL = new LinkedList();
-    //     testLL.toStringType();
-    //     expect(String(testLL.head)).toEqual('string');      
-    // });
+  // it(' ToString nodes ', () => {
+  //     let value = 'Nawal' ;
+  //     let testLL = new LinkedList();
+  //     testLL.toStringType();
+  //     expect(String(testLL.head)).toEqual('string');      
+  // });
 
-    it(' append() add new node at the end of the list ', () => {
-        let value = 'Nawal';
-        testLL.insert(value);
-        expect(testLL.head.value).toEqual('Nawal');
-        expect(testLL.head.value).toEqual(value);
-    });
+  it(' append() add new node at the end of the list ', () => {
+    let value = 'Nawal';
+    testLL.insert(value);
+    expect(testLL.head.value).toEqual('Nawal');
+    expect(testLL.head.value).toEqual(value);
+  });
 
-    it(' insertBefore() Insert Node before another Node ', () => {
-        let value1 = 'N1';
-        let value2 = 'N2';
-        testLL.insert(value1);
-        testLL.insert(value2);
-        testLL.insertBefore(value1, value2);
-        expect(testLL.head.value).toEqual(value2);
-        // expect(testLL.head.next).toEqual(value2)
-    });
+  it(' insertBefore() Insert Node before another Node ', () => {
+    let value1 = 'N1';
+    let value2 = 'N2';
+    testLL.insert(value1);
+    testLL.insert(value2);
+    testLL.insertBefore(value1, value2);
+    expect(testLL.head.value).toEqual(value2);
+    // expect(testLL.head.next).toEqual(value2)
+  });
 
-    it(' insertAfter() Insert node after given value ', () => {
-        let value1 = 'N1';
-        let value2 = 'N2';
-        testLL.insert(value1);
-        testLL.insert(value2);
-        testLL.insertAfter(value1, value2);
-        expect(testLL.head.value).toEqual(value2);
-    });
+  it(' insertAfter() Insert node after given value ', () => {
+    let value1 = 'N1';
+    let value2 = 'N2';
+    testLL.insert(value1);
+    testLL.insert(value2);
+    testLL.insertAfter(value1, value2);
+    expect(testLL.head.value).toEqual(value2);
+  });
 }); // end of LinkedList Module
--- __tests__/lib/node.test.js
+++ __tests__/lib/node.test.js
@@ -4,14 +4,14 @@ const Node = require('../../lib/node.js');
 
 describe(' Node Module ' , () =>
 {
-    it(' Node Constructor ' , () =>
-    {
-        let value = 'Nawal' ;
-        let testNode = new Node(value);
+  it(' Node Constructor ' , () =>
+  {
+    let value = 'Nawal' ;
+    let testNode = new Node(value);
 
-        expect(testNode.value).toEqual(value);
-        expect(testNode.next).toEqual(null);
+    expect(testNode.value).toEqual(value);
+    expect(testNode.next).toEqual(null);
 
-    }); // end of node constructor 
+  }); // end of node constructor 
 
 }); // end of node describe 
