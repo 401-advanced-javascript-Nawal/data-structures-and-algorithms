c50342fba618ad6a9a00a6cc090227f40f5096dd
Nsuliman <nsuliman88@gmail.com>
2020-01-28 19:41:01 +0200

    Done again coz of Jest issue

--- challenges/matrixFib/matFib.test.js
+++ challenges/matrixFib/matFib.test.js
@@ -4,21 +4,21 @@ const validator = require('./matFib.js');
 
 describe('validator module performs basic validation of', () => {
 
-    it('Summation Matrix Array  ', () => {
-        let arr = [ [1, 2, 3], [3, 5, 7], [1, 7, 10] ];4
-        let output = [6, 15, 18];
-        expect(validator.matrixSum(arr)).toEqual(output);
-      });
+  it('Summation Matrix Array  ', () => {
+    let arr = [ [1, 2, 3], [3, 5, 7], [1, 7, 10] ];4;
+    let output = [6, 15, 18];
+    expect(validator.matrixSum(arr)).toEqual(output);
+  });
     
-      it(' n must be Number ', () => {
-          let n =5;
-        expect(validator.isNumber(n)).toBeTruthy();
-      });
+  it(' n must be Number ', () => {
+    let n =5;
+    expect(validator.isNumber(n)).toBeTruthy();
+  });
 
-      it('Fibonacci Sequence ', () => {
-        let arr = [ 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55 ];
-        let n = 10;
-        expect(validator.fibFun(n)).toEqual(arr[n]);
-      });
+  it('Fibonacci Sequence ', () => {
+    let arr = [ 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55 ];
+    let n = 10;
+    expect(validator.fibFun(n)).toEqual(arr[n]);
+  });
     
-    });
+});
