e1570adc6194cca5aa28e251531c1bbfc1eeffdc
Nsuliman <nsuliman88@gmail.com>
2020-02-01 17:45:54 +0200

    all stck tests passed

--- __tests__/lib/stack.test.js
+++ __tests__/lib/stack.test.js
@@ -2,19 +2,22 @@
 
 const Stack = require('../../lib/stack.js');
 
-describe(' Stack Module ', () =>
-{
-    it('Push(), add item to stack using push()' , () =>
-    {
+describe(' Stack Module ', () => {
+    it('Push(), add item to stack using push()', () => {
+        // Can successfully instantiate an empty stack
+        // Can successfully push onto a stack
+        // Can successfully push multiple values onto a stack
         let newStack = new Stack();
+        expect(newStack.stackArr).toEqual([]);
         newStack.push('Sally');
         expect(newStack.peek()).toEqual('Sally');
         newStack.push('jasmine');
         expect(newStack.peek()).toEqual('jasmine');
     }); // end of push method testing
-    
-    it('Pop() , remove an item from stack using pop()' , () =>
-    {   
+
+    it('Pop() , remove an item from stack using pop()', () => {
+        // Can successfully pop off the stack
+        // Can successfully empty a stack after multiple pops
         let newStack = new Stack();
         newStack.push('Sally');
         newStack.push('jasmine');
@@ -24,12 +27,13 @@ describe(' Stack Module ', () =>
         // check the left element will be the top(peek)
         expect(newStack.peek()).toEqual('Sally');
         // check if there is no element to remove
-        newStack.pop(); 
+        newStack.pop();
         expect(newStack.stackArr).toEqual([]);
+        expect(newStack.top).toBeUndefined();
     }); // end of pop method testing 
 
-    it('Peek() , return the value( Top ) of the stack' , () =>
-    {
+    it('Peek() , return the value( Top ) of the stack', () => {
+        //Can successfully peek the next item on the stack
         let newStack = new Stack();
         newStack.push('Sally');
         // check the top of the stack 
