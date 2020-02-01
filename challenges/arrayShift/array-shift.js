1bbfb85f89d96cb02fcdd1db68100899c0a684f5
Nsuliman <nsuliman88@gmail.com>
2020-02-01 03:22:23 +0200

    eslint errors fixed

--- challenges/arrayShift/array-shift.js
+++ challenges/arrayShift/array-shift.js
@@ -4,8 +4,8 @@ let shiftArray = module.exports = {};
 
 shiftArray.insertShiftArray = (array,value) =>
 {
-    let middleIdx = Math.floor(array.length /2);
-    array.splice(middleIdx,0,value);
-    return array.length;
-}
+  let middleIdx = Math.floor(array.length /2);
+  array.splice(middleIdx,0,value);
+  return array.length;
+};
 
--- challenges/arrayShift/array-shift.test.js
+++ challenges/arrayShift/array-shift.test.js
@@ -4,10 +4,10 @@ const validator = require('./array-shift.js');
 
 describe('validator module performs basic validation of', () => {
 
-    it('insert a value into the middle of sorted array  ', () => {
+  it('insert a value into the middle of sorted array  ', () => {
 
-        let array = [4, 8, 15, 23, 42];
-        let value = 16;
-        expect(validator.insertShiftArray(array, value)).toEqual(6);
-    });
+    let array = [4, 8, 15, 23, 42];
+    let value = 16;
+    expect(validator.insertShiftArray(array, value)).toEqual(6);
+  });
 });
