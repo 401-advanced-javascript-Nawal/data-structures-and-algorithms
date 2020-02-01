3b6f7f57d7db90569ae7aabeeea72d021905b028
Nsuliman <nsuliman88@gmail.com>
2020-02-01 03:28:15 +0200

    eslint errors fixed

--- challenges/arrayBinarySearch/arrayBinarySearch.js
+++ challenges/arrayBinarySearch/arrayBinarySearch.js
@@ -3,36 +3,36 @@
 let binValidator = module.exports = {};
 
 
-let searchArr = [4, 8, 15, 16, 23, 42];
-let searchKey = 23;
+// let searchArr = [4, 8, 15, 16, 23, 42];
+// let searchKey = 23;
 
 // console.log('searchArr : ', searchArr);
 // console.log('searchKey : ', searchKey);
 
 binValidator.binarySearch =(array, key) =>{
 
-    let intialIdx = 0;
-    let endIdx = array.length-1;
+  let intialIdx = 0;
+  let endIdx = array.length-1;
 
-    while (intialIdx <= endIdx) {
+  while (intialIdx <= endIdx) {
 
-        let middle = Math.floor((intialIdx + endIdx) / 2);
-        // console.log('middle : ', middle);
+    let middle = Math.floor((intialIdx + endIdx) / 2);
+    // console.log('middle : ', middle);
 
-        if (array[middle] === key)
-            return middle;
+    if (array[middle] === key)
+      return middle;
 
-        if (array[middle] < key) {
-            intialIdx = middle + 1;
-            console.log('intialIdx : ', intialIdx);
-        }
-        else {
-            endIdx = middle - 1;
-            console.log('endIdx : ', endIdx);
-        }
+    if (array[middle] < key) {
+      intialIdx = middle + 1;
+      console.log('intialIdx : ', intialIdx);
     }
-    return false;
-}
+    else {
+      endIdx = middle - 1;
+      console.log('endIdx : ', endIdx);
+    }
+  }
+  return false;
+};
 
 // binarySearch(searchArr, searchKey)
 // console.log('binarySearch(searchArr, searchKey) : ', binarySearch(searchArr, searchKey));
--- challenges/arrayBinarySearch/arrayBinarySearch.test.js
+++ challenges/arrayBinarySearch/arrayBinarySearch.test.js
@@ -7,7 +7,7 @@ const validator = require('./arrayBinarySearch.js');
 
 describe('validator module performs basic validation of', () => {
 
-it('Binary Search for key  ', () => {
+  it('Binary Search for key  ', () => {
     let arr = [4, 8, 15, 16, 23, 42];
     let key = 23;
     expect(validator.binarySearch(arr,key)).toEqual(4);
