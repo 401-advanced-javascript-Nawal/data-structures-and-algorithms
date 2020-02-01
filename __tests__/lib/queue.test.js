e3fefcb74a60b63203bf6de42671d7de9df0d47b
Nsuliman <nsuliman88@gmail.com>
2020-02-01 21:52:44 +0200

    queue tests passed

---
+++ __tests__/lib/queue.test.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const Queue = require('../../lib/queue.js');
+
+describe(' Queue Module ', () => {
+
+    it('enqueue() , Add an item at the end of the queue ', () => {
+        // Can successfully enqueue into a queue
+        // Can successfully enqueue multiple values into a queue
+
+        let newQueue = new Queue();
+        newQueue.enqueue('Sally');
+        newQueue.enqueue('jasmine');
+        expect(newQueue.front).toEqual('Sally');
+        expect(newQueue.rear).toEqual('jasmine');
+    }); // end of enqueue method test 
+
+    it('dequeue() , remove an item from the front of the queue ', () => {
+        // Can successfully empty a queue after multiple dequeues
+        let newQueue = new Queue();
+        // console.log('newQueue : ', newQueue);
+        // expect(newQueue.queueArr.length).toBe(null);        
+        newQueue.enqueue('Sally');
+        newQueue.enqueue('jasmine');
+        let deItem = newQueue.dequeue();
+        expect(deItem).toEqual('Sally');
+        expect(newQueue.front).toEqual('jasmine');
+        expect(newQueue.rear).toEqual('jasmine');
+        newQueue.dequeue();
+        // Can successfully instantiate an empty queue
+        expect(newQueue.queueArr.length).toBe(0);
+        expect(newQueue.front).toBeNull();
+        expect(newQueue.rear).toBeNull();
+
+    }); // end of enqueue method test
+
+
+}); // end of stack testing 
