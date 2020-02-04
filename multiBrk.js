0933f181875b775413659261afad52d2decab5d3
Nsuliman <nsuliman88@gmail.com>
2020-02-05 01:28:41 +0200

    eslint errors fixed

--- multiBrk.js
+++ multiBrk.js
@@ -4,36 +4,36 @@
 // let stringBr = '{}{Code}[Fellows](())';
 
 class Multi {
-    constructor(stringBr){
-        this.stringBr = stringBr;
+  constructor(stringBr){
+    this.stringBr = stringBr;
+  }
+
+  multiBraket(str){
+
+    let spiltStr = str.split('');
+    // console.log('spiltStr : ', spiltStr);
+    let braketStrCount = 0;
+    spiltStr.forEach(element => {
+      // console.log('element : ', element);
+      if(element === '(' || element === ')')
+      {
+        braketStrCount++;
+      } else if(element === '{' || element === '}')
+      {
+        braketStrCount++;
+      } else if(element === '[' || element === ']')
+      {
+        braketStrCount++;
+      }
+    });
+    console.log('braketStrCount : ', braketStrCount);
+    if(braketStrCount%2 === 0)
+    {
+      return true;
+    }else {
+      return false;
     }
-
-    multiBraket(str){
-
-        let spiltStr = str.split('');
-        // console.log('spiltStr : ', spiltStr);
-        let braketStrCount = 0;
-        spiltStr.forEach(element => {
-            // console.log('element : ', element);
-            if(element === '(' || element === ')')
-            {
-                braketStrCount++;
-            } else if(element === '{' || element === '}')
-            {
-                braketStrCount++;
-            } else if(element === '[' || element === ']')
-            {
-                braketStrCount++;
-            }
-        });
-        console.log('braketStrCount : ', braketStrCount);
-        if(braketStrCount%2 === 0)
-        {
-            return true;
-        }else {
-            return false;
-        }
-    } // end of multi function
+  } // end of multi function
 
 }
  
