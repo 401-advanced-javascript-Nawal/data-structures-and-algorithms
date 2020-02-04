599ff69900047c851a5f8d10b95ad5532217c5f0
Nsuliman <nsuliman88@gmail.com>
2020-02-05 01:14:51 +0200

    tests passed

--- multiBrk.js
+++ multiBrk.js
@@ -1,32 +1,41 @@
 'use strict';
 
 
-let stringBr = '{}{Code}[Fellows](())';
-function multiBraket(str){
+// let stringBr = '{}{Code}[Fellows](())';
 
-    let spiltStr = str.split('');
-    // console.log('spiltStr : ', spiltStr);
-    let braketStrCount = 0;
-    spiltStr.forEach(element => {
-        // console.log('element : ', element);
-        if(element === '(' || element === ')')
-        {
-            braketStrCount++;
-        } else if(element === '{' || element === '}')
-        {
-            braketStrCount++;
-        } else if(element === '[' || element === ']')
-        {
-            braketStrCount++;
-        }
-    });
-    console.log('braketStrCount : ', braketStrCount);
-    if(braketStrCount%2 === 0)
-    {
-        return 'even';
-    }else {
-        return 'odd';
+class Multi {
+    constructor(stringBr){
+        this.stringBr = stringBr;
     }
-} // end of multi function 
 
-multiBraket(stringBr);
+    multiBraket(str){
+
+        let spiltStr = str.split('');
+        // console.log('spiltStr : ', spiltStr);
+        let braketStrCount = 0;
+        spiltStr.forEach(element => {
+            // console.log('element : ', element);
+            if(element === '(' || element === ')')
+            {
+                braketStrCount++;
+            } else if(element === '{' || element === '}')
+            {
+                braketStrCount++;
+            } else if(element === '[' || element === ']')
+            {
+                braketStrCount++;
+            }
+        });
+        console.log('braketStrCount : ', braketStrCount);
+        if(braketStrCount%2 === 0)
+        {
+            return true;
+        }else {
+            return false;
+        }
+    } // end of multi function
+
+}
+ 
+module.exports = Multi;
+// multiBraket(stringBr);
---
+++ multiBrk.test.js
@@ -0,0 +1,62 @@
+'use strict';
+
+const Mult = require('./multiBrk.js');
+
+describe(' Multi Braket Validation' , () =>
+{
+    it('{} return true' , () =>
+    {
+        let str = '{}';
+        const mm = new Mult(str);
+        expect(mm.multiBraket(str)).toBeTruthy();
+    });
+
+    it('{}(){} return true' , () =>
+    {
+        let str = '{}(){}';
+        const mm = new Mult(str);
+        expect(mm.multiBraket(str)).toBeTruthy();
+    });
+
+    it('()[[Extra Characters]] return true' , () =>
+    {
+        let str = '()[[Extra Characters]]';
+        const mm = new Mult(str);
+        expect(mm.multiBraket(str)).toBeTruthy();
+    });
+
+    it('(){}[[]] return true' , () =>
+    {
+        let str = '(){}[[]]';
+        const mm = new Mult(str);
+        expect(mm.multiBraket(str)).toBeTruthy();
+    });
+
+    it('{}{Code}[Fellows](()) return true' , () =>
+    {
+        let str = '{}{Code}[Fellows](())';
+        const mm = new Mult(str);
+        expect(mm.multiBraket(str)).toBeTruthy();
+    });
+
+    it('[({}] return false' , () =>
+    {
+        let str = '[({}]';
+        const mm = new Mult(str);
+        expect(mm.multiBraket(str)).toBeFalsy();
+    });
+
+    it('(](	 return false' , () =>
+    {
+        let str = '(](';
+        const mm = new Mult(str);
+        expect(mm.multiBraket(str)).toBeFalsy();
+    });
+
+    it('{(}) return true' , () =>
+    {
+        let str = '{(})';
+        const mm = new Mult(str);
+        expect(mm.multiBraket(str)).toBeTruthy();
+    });
+}); // end of multi testing 
