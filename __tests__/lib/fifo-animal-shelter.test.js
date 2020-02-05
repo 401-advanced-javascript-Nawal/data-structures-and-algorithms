3b02df18699b9f48d2622387ed149fb34a0c94de
Nsuliman <nsuliman88@gmail.com>
2020-02-05 04:20:20 +0200

    starting js file

---
+++ __tests__/lib/fifo-animal-shelter.test.js
@@ -0,0 +1,26 @@
+'use strict';
+
+
+describe(' Animal Shelter Module ' , () =>
+{
+    it(' Add animal when it\'s cat ' , () =>{
+
+    });
+
+    it(' Add animal when it\'s dog ' , () =>{
+
+    });
+
+    it(' remove pref when it\'s cat ' , () =>{
+
+    });
+
+    it(' remove pref when it\'s dog ' , () =>{
+
+    });
+
+    it(' Return Null when pref is not an animal ' , () =>{
+
+    });
+
+}); // end of AnimalShelter test 
