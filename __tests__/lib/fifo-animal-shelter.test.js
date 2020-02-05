0fd467bfe5916e446af154a2721f5b43cde478d5
Nsuliman <nsuliman88@gmail.com>
2020-02-05 04:53:43 +0200

    enqueue method done

--- __tests__/lib/fifo-animal-shelter.test.js
+++ __tests__/lib/fifo-animal-shelter.test.js
@@ -5,18 +5,23 @@ const AnimalShelter = require('../../lib/fifo-animal-shelter.js');
 let aniSh = new AnimalShelter();
 
 describe(' Animal Shelter Module ', () => {
-    let animal1 = { name: 'Luna', type: 'cat' };
-    let animal2 = { name: 'Cooper', type: 'dog' };
+    let animal1 = ['Luna','cat'] ;
+    let animal2 = ['Cooper','dog'];
     let anyth = { name: 'ala bali', type: 'song' };
 
 
     it(' Add animal when it\'s cat ', () => {
+        // console.log('aniSh : ', aniSh);
         aniSh.enqueue(animal1);
-        // expect(aniSh.n)
+        // console.log('animal1 : ', animal1);
+        expect(aniSh.catArr[0][0]).toBe('Luna');
+        expect(aniSh.catArr[0][1]).toBe('cat');
     });
 
     it(' Add animal when it\'s dog ', () => {
-
+        aniSh.enqueue(animal2);
+        expect(aniSh.dogArr[0][0]).toBe('Cooper');
+        expect(aniSh.dogArr[0][1]).toBe('dog');
     });
 
     it(' remove pref when it\'s cat ', () => {
