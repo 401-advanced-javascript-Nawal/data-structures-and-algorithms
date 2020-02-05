57c27ae0e62b467c1aff3d594a4f4f6fd362d4f0
Nsuliman <nsuliman88@gmail.com>
2020-02-05 04:39:12 +0200

    class done

--- __tests__/lib/fifo-animal-shelter.test.js
+++ __tests__/lib/fifo-animal-shelter.test.js
@@ -1,25 +1,33 @@
 'use strict';
 
+const AnimalShelter = require('../../lib/fifo-animal-shelter.js');
 
-describe(' Animal Shelter Module ' , () =>
-{
-    it(' Add animal when it\'s cat ' , () =>{
+let aniSh = new AnimalShelter();
+
+describe(' Animal Shelter Module ', () => {
+    let animal1 = { name: 'Luna', type: 'cat' };
+    let animal2 = { name: 'Cooper', type: 'dog' };
+    let anyth = { name: 'ala bali', type: 'song' };
+
+
+    it(' Add animal when it\'s cat ', () => {
+        aniSh.enqueue(animal1);
+        // expect(aniSh.n)
+    });
+
+    it(' Add animal when it\'s dog ', () => {
 
     });
 
-    it(' Add animal when it\'s dog ' , () =>{
+    it(' remove pref when it\'s cat ', () => {
 
     });
 
-    it(' remove pref when it\'s cat ' , () =>{
+    it(' remove pref when it\'s dog ', () => {
 
     });
 
-    it(' remove pref when it\'s dog ' , () =>{
-
-    });
-
-    it(' Return Null when pref is not an animal ' , () =>{
+    it(' Return Null when pref is not an animal ', () => {
 
     });
 
