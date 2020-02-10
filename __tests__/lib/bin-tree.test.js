c4ffe4f1b8d41a2e3e0ebb04e9d0fca9d69a414f
Nsuliman <nsuliman88@gmail.com>
2020-02-10 02:01:17 +0200

    starts tests

---
+++ __tests__/lib/bin-tree.test.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const Bintree = require('../../lib/bin-tree.js');
+const Node = require('../../lib/tree.js');
+
+describe(' Binary Tree Modular' , () =>{
+    
+}) //  end of binary tree test 
+
