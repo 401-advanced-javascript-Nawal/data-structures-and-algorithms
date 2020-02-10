f644e1b6897f50fac518d7827b2cc366c0f4516b
Nsuliman <nsuliman88@gmail.com>
2020-02-10 08:36:36 +0200

    tests

--- __tests__/lib/bin-tree.test.js
+++ __tests__/lib/bin-tree.test.js
@@ -4,6 +4,22 @@ const Bintree = require('../../lib/bin-tree.js');
 const Node = require('../../lib/tree.js');
 
 describe(' Binary Tree Modular' , () =>{
-    
+    let tree = [];
+    let one = new Node(10);
+    let two = new Node(20);
+    let three = new Node(30);
+    let four = new Node(40);
+
+    one.left = three;
+    one.right = two;
+    two.left = four;
+
+    tree = new Bintree(one);
+
+    it('PreOrder()' , ()=>{
+        console.log('tree : ', tree);
+        expect(tree.preOder()).toEqual(['one','three','four','two']);
+    }); // end of preorder 
+
 }) //  end of binary tree test 
 
